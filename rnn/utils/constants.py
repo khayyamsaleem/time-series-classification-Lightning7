3784da70069fbd6a2e8bcc298559f0a9c048042b
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-04-30 08:41:27 -0400

    added rnn

---
+++ rnn/utils/constants.py
@@ -0,0 +1,30 @@
+
+TRAIN_FILES = ['../data\\Adiac_TRAIN', # 0
+               '../data\\ArrowHead_TRAIN',  # 1
+               '../data\\ChlorineConcentration_TRAIN',  # 2
+               '../data\\InsectWingbeatSound_TRAIN',  # 3
+               '/data/Lighting7/Lighting7_TRAIN'  # 4
+               ]
+
+TEST_FILES = ['../data\\Adiac_TEST', # 0
+              '../data\\ArrowHead_TEST', # 1
+              '/data/ChlorineConcentration_TEST', # 2
+              '../data\\InsectWingbeatSound_TEST', # 3
+              '../data/Lighting7/Lighting7_TEST', # 4
+              ]
+
+MAX_SEQUENCE_LENGTH_LIST = [176, # 0
+                            251, # 1
+                            166, # 2
+                            256, # 3
+                            319, # 4
+                            234, # 5
+                            ]
+
+NB_CLASSES_LIST = [37, # 0
+                   3, # 1
+                   3, # 2
+                   11, # 3
+                   7, # 4
+                   2, # 5
+                   ]
